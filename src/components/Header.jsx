0b0a23df396a3957c63eb5f4f976bd25092f132d
RockurDev <babuse4ka2002@yandex.ru>
2023-02-01 15:14:19 +0300

    add components to main page

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQB-nqRycJhFNrnzpkQ3oI6vWvslHm71jjXAg&usqp=CAU' alt='1'></img>
+    </header>
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+
+const Profile = () => {
+    return (
+        <div className='content'>
+                <div>
+                    <img src='https://images.unsplash.com/photo-1664023937999-5937f62d0f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2489&q=80' alt='2'></img>
+                </div>
+
+                <div>
+                    <img scr='https://images.unsplash.com/photo-1618641986557-1ecd230959aa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aW5zdGFncmFtJTIwcHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80' alt='ava'></img>
+                    ava + desc
+                </div>
+
+                <div>
+                    my posts
+                </div>
+
+                <div>
+                    new post
+                </div>
+
+
+            </div>
+    )
+};
+
+export default Profile;
